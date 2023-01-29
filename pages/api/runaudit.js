9167b75d485dbbce654d68be50f9522e80bd80c3
Elakkuvan Rajamani <elakkuvan.rajamani@valtech.com>
2023-01-29 23:16:27 +0100

    added additional pages and api end points

---
+++ pages/api/runaudit.js
@@ -0,0 +1,5 @@
+export default async function handler(req, res) {
+    console.log("Inside RunAudit API Function");  
+    res.json({ message: "completed" });
+  }
+  
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+export default function Index() {
+  return (
+    <div className="bg-white dark:bg-gray-800 flex relative z-20 items-center">
+      <div className="container mx-auto px-6 flex flex-col justify-between items-center relative py-8">
+        <div className="flex flex-col">
+          <h1 className="font-light w-full uppercase text-center text-4xl sm:text-5xl dark:text-white text-gray-800">
+            Vercel Puppeteer Page
+          </h1>
+        </div>
+      </div>
+    </div>
+  );
+}
