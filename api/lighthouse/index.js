eb1051e9d7e8ff272372089bdbc760c16571f776
Elakkuvan Rajamani <elakkuvan.rajamani@valtech.com>
2023-01-30 00:52:00 +0100

    check async fun

--- api/lighthouse/index.js
+++ api/lighthouse/index.js
@@ -10,6 +10,12 @@ if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
   puppeteer = require("puppeteer");
 }
 
+async function loopMe() {
+  for (let i = 0; i <= 100000; i++) {
+    console.log("i value", i);
+  }
+}
+
 app.get("/api/lighthouse", async (req, res) => {
   let options = {};
 
@@ -24,10 +30,12 @@ app.get("/api/lighthouse", async (req, res) => {
   }
 
   try {
-    let browser = await puppeteer.launch(options);
-    let page = await browser.newPage();
-    await page.goto("https://xm-cloud-integration.vercel.app/About");
-    res.send(await page.title());
+    res.status(200).json("I will continue to work.");
+    await loopMe();
+    // let browser = await puppeteer.launch(options);
+    // let page = await browser.newPage();
+    // await page.goto("https://xm-cloud-integration.vercel.app/About");
+    // res.send(await page.title());
   } catch (err) {
     console.error(err);
     return null;
